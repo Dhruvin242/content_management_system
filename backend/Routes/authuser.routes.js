faaacd90f1e10b3b0dca0d5ed8444b00a2ac31f9
Dhruvin242 <dhruvin12modi@gmail.com>
2022-11-04 09:52:50 +0530

    all thing deleted recreate

--- backend/Routes/authuser.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const userAuthContoller = require("../Controllers/userAuth.controller");
-const routes = express.Router();
-
-routes.post("/register", userAuthContoller.register);
-routes.post("/login", userAuthContoller.login);
-
-module.exports = routes;
